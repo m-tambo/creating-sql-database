78b2fa529ab635c7ba0e2d1dc89ebefbb01ab2bf
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-08 00:09:31 -0600

    create table function

--- sqlite3.js
+++ sqlite3.js
@@ -1,1 +1,6 @@
 'use strict'
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('fakeCompany.sqlite')
+
+db.run(`CREATE TABLE IF NOT EXISTS employees (id INT, first_name TEXT, last_name TEXT, title TEXT, age INT, sign TEXT)`)
