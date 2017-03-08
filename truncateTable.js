fef4f5b1c5c8fa2ba71d73145c25744ab517aee5
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-08 11:37:13 -0600

    added function to truncate table

---
+++ truncateTable.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('fakeCompany.sqlite')
+
+const truncate = () => {
+  db.run(`DELETE FROM employees WHERE id >= 1`)
+}
+
+truncate()
