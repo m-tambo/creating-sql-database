093088fa6a4f8e1bf480d48f8272d6d3bb492ec3
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-08 11:40:58 -0600

    edited truncate function

--- truncateTable.js
+++ truncateTable.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('fakeCompany.sqlite')
 
 const truncate = () => {
-  db.run(`DELETE FROM employees WHERE id >= 1`)
+  db.run(`DELETE FROM employees WHERE id > 0`)
 }
 
 truncate()
